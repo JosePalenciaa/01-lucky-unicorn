15c1ff59506eb3666d13572492ec06e4158eb388
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2023-03-06 15:00:01 +1300

    Add files via upload

---
+++ 01_lucky_unicorn/01_lucky_unicorn/00_looping.py
@@ -0,0 +1,5 @@
+# for item in range(0, 20, +4):  print(item)
+options = ["Zebra", "Donkey", "Giraffe", "Unicorn", "Horse"]
+
+for animal in options:
+    print(animal)
---
+++ 01_lucky_unicorn/01_lucky_unicorn/01_yes_no_v1.py
@@ -0,0 +1,17 @@
+# Ask the user if they have played before
+display_instructions = input("Have you played before? ").lower()
+
+# If they say 'yes', output 'program continues'
+if display_instructions == "yes":
+    print("You selected 'yes'")
+    print("Program continues")
+
+# If they say 'no', output 'display instructions'
+elif display_instructions == "no":
+    print("You selected 'no'")
+    print("Display instructions")
+
+# If they don't respond with either yes or no
+
+else:
+    print("Please answer 'yes' or 'no'")
